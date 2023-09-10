b15394f3bc28f309441e396f49c0464ca5a4ac89
martin-bachmann <martinpezbach@gmail.com>
2023-09-10 12:54:22 -0300

    feat: Create second view and URL

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("january", views.index)]
+urlpatterns = [
+    path("january", views.january),
+    path("february", views.february),
+]
